f15da7e8737557fd50b4c32d47934626077dee2c
Rodrigo Solis <solisrodrigo619@gmail.com>
2025-08-04 17:44:34 -0300

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -25,8 +25,7 @@ namespace Yahtzee
          int variablG=0;
 
 
-    
- 
+
         string[] rutasDados = { "cara1.png", "cara2.png", "cara3.png", "cara4.png", "cara5.png", "cara6.png" };
      
         bool[] retenido = new bool[5];
@@ -304,6 +303,7 @@ namespace Yahtzee
         }
     }
 }
+
 
 
 
